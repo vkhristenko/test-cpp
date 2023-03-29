aac9a8fdd67ee227067e05c827a850b04029da28
vkhristenko <vdkhristenko1991@gmail.com>
2023-03-29 14:15:48 +0200

    dummy protoc plugin

--- src/protos/plugins/dummy/main.cpp
+++ src/protos/plugins/dummy/main.cpp
@@ -1,6 +1,13 @@
+#include <sstream>
+
 #include <google/protobuf/compiler/code_generator.h>
 #include <google/protobuf/compiler/cpp/cpp_generator.h>
 #include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/stubs/stl_util.h>
+#include <google/protobuf/compiler/plugin.pb.h>
 
 #include "core/macros.h"
 #include "fmt/core.h"
@@ -11,9 +18,34 @@ using namespace google::protobuf::compiler;
 class DummyGenerator : public CodeGenerator {
 public:
     bool Generate(const FileDescriptor* file, const std::string& parameter,
-                  GeneratorContext* generator_context,
+                  GeneratorContext* ctx,
                   std::string* error) const override {
-        ::fmt::print("{}", TCPP_PRETTY_FUNCTION());
+        auto name = file->name();
+        auto dot_pos = name.find_last_of(".");
+        auto name_wo_ext = name.substr(0, dot_pos);
+        std::unique_ptr<io::ZeroCopyOutputStream> stream{
+            ctx->Open(name_wo_ext + ".dummy")};
+
+        std::stringstream ss;
+        for (int i=0; i<file->message_type_count(); i++) {
+            auto desc = file->message_type(i);
+            auto header = ::fmt::format(
+                "Header:\nname = {} Full Name = {}\n", desc->name(), desc->full_name()
+            );
+            ss << header;
+
+            for (int j=0; j<desc->field_count(); j++) {
+                auto f = desc->field(j);
+                auto f_str = ::fmt::format(
+                    "name = {} type = {}\n",
+                    f->name(), f->type_name());
+                ss << f_str;
+            }
+        }
+
+        google::protobuf::io::Printer printer(stream.get(), '$');
+        printer.PrintRaw(ss.str());
+
         return true;
     }
 };
