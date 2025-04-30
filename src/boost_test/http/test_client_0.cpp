5f48ed995d737f64c84c493a39d5e5c8cc144430
vkhristenko <vdkhristenko1991@gmail.com>
2025-05-01 00:05:14 +0200

    main testing

---
+++ src/boost_test/http/test_client_0.cpp
@@ -0,0 +1,39 @@
+#include "bext/utils/aliases.h"
+
+#include <iostream>
+#include <memory>
+
+struct ClientSession : std::enable_shared_from_this<ClientSession> {
+    ClientSession(::basio::io_context& ctx, ::basio::ip::tcp::endpoint&& endpoint)
+        : ctx_{ctx}
+        , endpoint_{std::move(endpoint)}
+    {
+    }
+
+    void start() {
+    }
+
+    ::basio::io_context& ctx_;
+    ::basio::ip::tcp::endpoint endpoint_;
+};
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        std::cout << "./exe address port" << std::endl;
+        return 1;
+    }
+
+    auto address = ::basio::ip::make_address(argv[1]);
+    auto const port = static_cast<uint16_t>(std::atoi(argv[2]));
+
+    ::basio::io_context ctx;
+
+    auto client_session = std::make_shared<ClientSession>(ctx, ::basio::ip::tcp::endpoint{address, port});
+    client_session->start();
+
+    while (1) {
+        ctx.poll();
+    }
+
+    return 0;
+}
---
+++ src/boost_test/http/test_server_0.cpp
@@ -0,0 +1,39 @@
+#include "bext/utils/aliases.h"
+
+#include <iostream>
+#include <memory>
+
+struct Listener : std::enable_shared_from_this<Listener> {
+    Listener(::basio::io_context& ctx, ::basio::ip::tcp::endpoint&& endpoint)
+        : ctx_{ctx} 
+        , endpoint_{std::move(endpoint)}
+    {
+    }
+
+    void start() {
+    }
+
+    ::basio::io_context& ctx_;
+    ::basio::ip::tcp::endpoint endpoint_;
+};
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        std::cout << "./exe address port" << std::endl;
+        return 1;
+    }
+
+    auto address = ::basio::ip::make_address(argv[1]);
+    auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
+
+    ::basio::io_context ctx;
+
+    auto listener = std::make_shared<Listener>(ctx, ::basio::ip::tcp::endpoint{address, port});
+    listener->start();
+
+    while (true) {
+        ctx.poll();
+    }
+
+    return 0;
+}
